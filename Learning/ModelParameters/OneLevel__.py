950e7342508b4a5000b2be6e013f7856395b0095
larvasapiens <sebasnr95@gmail.com>
2016-06-05 18:17:10 -0500

    Added support for models to load parameters from an external file

---
+++ Learning/ModelParameters/OneLevel__.py
@@ -0,0 +1,38 @@
+bestScores = [0.0]
+bestFindings = [
+    {
+        'generalTM': {
+            'columnDimensions': (2, 6048),
+            'cellsPerColumn': 64,
+            'initialPermanence': 0.263488191214,
+            'connectedPermanence': 0.674714438958,
+            'minThreshold': 4,
+            'maxNewSynapseCount': 4,
+            'permanenceDecrement': 0.15,
+            'permanenceIncrement': 0.117671359444,
+            'predictedSegmentDecrement': 0.0,
+            'activationThreshold': 1,
+            'seed': 42
+        },
+        'generalSP': {
+            'columnDimensions': (24,),
+            'inputDimensions': (264,),
+            'potentialRadius': 297,
+            'potentialPct': 0.726248028695,
+            'globalInhibition': True,
+            'localAreaDensity': 0.02,
+            'numActiveColumnsPerInhArea': -1.0,
+            'stimulusThreshold': 2,
+            'synPermInactiveDec': 0.165088154764,
+            'synPermActiveInc': 0.1,
+            'synPermConnected': 0.236217765977,
+            'minPctOverlapDutyCycle': 0.302204519404,
+            'minPctActiveDutyCycle': 0.0,
+            'dutyCyclePeriod': 9,
+            'maxBoost': 1,
+            'seed': 42,
+            'spVerbosity': 0,
+            'wrapAround': True
+        }
+    }
+]
