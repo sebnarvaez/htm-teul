81dcb713063b792d6f04d7d66f686c0bd0855edc
larvasapiens <sebasnr95@gmail.com>
2017-02-08 17:00:56 -0500

    Added command-line args support. Added Screenshot

--- main.py
+++ main.py
@@ -23,10 +23,12 @@
 #  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 import sys
+import getopt
 import cPickle
 from Utils import TestSuite
 from PyQt5.QtWidgets import QApplication
 from GUI.MainWindow import MainWindow
+
 from Learning.EncoderFactory import *
 from Utils.CustomCategoryEncoder import CustomCategoryEncoder
 
@@ -38,77 +40,188 @@ from Learning import SpanishTestSet
 from Learning import EnglishTrainingSet
 from Learning import EnglishTestSet
 
-from Learning.LearningModels.FeedbackModel import FeedbackModel as CurrentModel
-import Learning.ModelParameters.Feedback81 as BestResults
+from Learning.LearningModels.ClassicModel import ClassicModel
+from Learning.LearningModels.OneLevelModel import OneLevelModel
+from Learning.LearningModels.FeedbackModel import FeedbackModel
+
+from Learning.ModelParameters import Classic80
+from Learning.ModelParameters import OneLevel90
+from Learning.ModelParameters import Feedback81
+
+dataSets = {
+    'Total': (TotalTrainingSet, TotalTrainingSet),
+    'Partial': (PartialTrainingSet, PartialTestSet),
+    'Spanish': (SpanishTrainingSet, SpanishTestSet),
+    'English': (EnglishTrainingSet, EnglishTestSet)
+}
+models = {
+    'Classic': ClassicModel,
+    'OneLevel': OneLevelModel,
+    'Feedback': FeedbackModel
+}
+modelParams = {
+    'Classic': Classic80,
+    'OneLevel': OneLevel90,
+    'Feedback': Feedback81
+}
+
+# Assign default values
+setsName = 'Total'
+enc = 'cce'
+modelName = 'OneLevel'
+iterations = 30
+
+whether_gui = True
+whether_tests = False
+whether_save = False
+
+def parse_parameters():
+    global dataSets
+    global models
+
+    global setsName
+    global enc
+    global modelName
+    global iterations
+    global whether_gui
+    global whether_tests
+    global whether_save
+
+    try:
+        opts, args = getopt.getopt(
+            sys.argv[1:],
+            "hd:e:m:i:ts",
+            ["help", "dataset=", "encoder=", "model=", "iterations=", "no-gui",
+                "tests", "save"]
+        )
+    except getopt.GetoptError, e:
+        print('An error occurred reading command-line args: ', e)
+
+    if len(opts) == 0:
+        print('No arguments passed. Using defaults...')
+
+    incorrect_args_str = "Incorrect value for {}. Use -h or --help to show allowed values"
+
+    # Collect and validate command-line args
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            print("""
+HTM-TEUL Help:
+  OPTIONS:
+    -h | --help: To show this help
+    -d | --dataset: Can be 'Total'*, 'Partial', 'Spanish' or 'English'
+    -e | --encoder: Can be 'rle', 'tre' or 'cce'*
+    -m | --model: Can be 'Classic', 'OneLevel'* or 'Feedback
+    -i | --iterations: Number of times the training data will be passed to the
+        model. Must be an integer (default=30).
+    -t | --tests: Run the TestSuite.
+    -s | --save: Save the trained model. Note that the resulting files can be 
+heavy (around 50mb).
+    --no-gui: Do not display gui
+
+Test results and saved models will be stored in Reults/ folder.
+*=default
+""")
+            sys.exit()
+        elif opt in ("-d", "--dataset"):
+            if arg in dataSets.keys():
+               setsName = arg
+            else:
+                print(incorrect_args_str.format('dataset'))
+                sys.exit(2)
+        elif opt in ("-e", "--encoder"):
+            if arg in ('rle', 'tre', 'cce'):
+                enc = arg
+            else:
+                print(incorrect_args_str.format('encoder'))
+                sys.exit(2)
+        elif opt in ("-m", "--model"):
+            if arg in models.keys():
+               modelName = arg
+            else:
+                print(incorrect_args_str.format('model'))
+                sys.exit(2)
+        elif opt in ("-i", "--iterations"):
+            if arg.isdigit():
+               iterations = int(arg)
+            else:
+                print(incorrect_args_str.format('iterations'))
+                sys.exit(2)
+    opt_names = [opt for opt, arg in opts]
+    whether_gui = not "--no-gui" in opt_names
+    whether_tests = "-t" in opt_names or "--tests" in opt_names
+    whether_save = "-s" in opt_names or "--save" in opt_names
 
 if __name__ == '__main__':
-    for currentSets in ((TotalTrainingSet, TotalTrainingSet, 'Total'),
-                        (PartialTrainingSet, PartialTestSet, 'Partial'),
-                        (SpanishTrainingSet, SpanishTestSet, 'Spanish'),
-                        (EnglishTrainingSet, EnglishTestSet, 'English')):
-        trainingSet = currentSets[0]
-        testSet = currentSets[1]
-        setsName = currentSets[2]
 
-        for enc in ('rle', 'tre', 'cce'):
+    parse_parameters()
 
-            if enc == 'rle':
-                abcLength = 26
-                bitsPerLetter = 3
-                maxWordLength = 20
-                randomBits = bitsPerLetter * maxWordLength
+    currentSets = dataSets[setsName]
+    CurrentModel = models[modelName]
+    BestParameters = modelParams[modelName]
 
-                # Mantain sparsity of 10%  in the random bits
-                rleWidth = (abcLength * bitsPerLetter * maxWordLength) +\
-                        (randomBits * 10)
-                wordEncoder = actionEncoder = RandomizedLetterEncoder(rleWidth,
-                    randomBits, bitsPerLetter)
+    trainingSet = currentSets[0]
+    testSet = currentSets[1]
 
-            elif enc == 'tre':
-                wordEncoder = actionEncoder = TotallyRandomEncoder(1024, 204)
+    if enc == 'rle':
+        abcLength = 26
+        bitsPerLetter = 3
+        maxWordLength = 20
+        randomBits = bitsPerLetter * maxWordLength
 
-            elif enc == 'cce':
-                wordEncoder = CustomCategoryEncoder(
-                    21,
-                    list(trainingSet.categories[
-                            trainingSet.inputIdx['wordInput']
-                        ]),
-                    nAdditionalCategorySlots=15,
-                )
-                actionEncoder = CustomCategoryEncoder(
-                    21,
-                    list(trainingSet.categories[
-                            trainingSet.inputIdx['actionInput']
-                        ]),
-                    nAdditionalCategorySlots=15,
-                )
+        # Mantain sparsity of 10%  in the random bits
+        rleWidth = (abcLength * bitsPerLetter * maxWordLength) +\
+                (randomBits * 10)
+        wordEncoder = actionEncoder = RandomizedLetterEncoder(rleWidth,
+            randomBits, bitsPerLetter)
 
-            encoderName = wordEncoder.__class__.__name__
+    elif enc == 'tre':
+        wordEncoder = actionEncoder = TotallyRandomEncoder(1024, 204)
 
-            model = CurrentModel(wordEncoder, actionEncoder, trainingSet,
-                BestResults.bestFindings[0])
-            modelName = model.__class__.__name__
+    elif enc == 'cce':
+        wordEncoder = CustomCategoryEncoder(
+            21,
+            list(trainingSet.categories[
+                    trainingSet.inputIdx['wordInput']
+                ]),
+            nAdditionalCategorySlots=15,
+        )
+        actionEncoder = CustomCategoryEncoder(
+            21,
+            list(trainingSet.categories[
+                    trainingSet.inputIdx['actionInput']
+                ]),
+            nAdditionalCategorySlots=15,
+        )
 
-            print(modelName)
-            print(encoderName)
-            model.train(30, maxTime=-1, verbosity=1)
+    encoderName = wordEncoder.__class__.__name__
 
-            fileName = 'Results/'
-            # Strips the 'Model' fron the name
-            fileName += modelName[:-5] + setsName + '-'
-            # Appends only the Capital letters
-            fileName += ''.join(cap for cap in encoderName if cap.isupper())
-            #fileName += 'OneRegionExp32'
+    model = CurrentModel(wordEncoder, actionEncoder, trainingSet,
+        BestParameters.bestFindings[0])
+    modelName = model.__class__.__name__
 
-            TestSuite.testModel(model, testSet.trainingData,
-                fileName=(fileName + '_Results'))
+    print(modelName)
+    print(encoderName)
+    model.train(iterations, maxTime=-1, verbosity=1)
 
-            #print("Saving the model...")
-            #with open((fileName + '.pck'), 'wb') as modelFile:
-            #    cPickle.dump(model, modelFile, -1)
-            #print("Done!")
+    fileName = 'Results/'
+    # Strips the 'Model' fron the name
+    fileName += modelName[:-5] + setsName + '-'
+    # Appends only the Capital letters
+    fileName += ''.join(cap for cap in encoderName if cap.isupper())
+    #fileName += 'OneRegionExp32'
 
-            #app = QApplication([])
-            #window = MainWindow(model)
-            #app.exec_()
-            #sys.exit(app.exec_())
+    if whether_tests:
+        TestSuite.testModel(model, testSet.trainingData,
+            fileName=(fileName + '_Results'))
+
+    if whether_save:
+        print("Saving the model...")
+        with open((fileName + '.pck'), 'wb') as modelFile:
+            cPickle.dump(model, modelFile, -1)
+        print("Done!")
+
+    if whether_gui:
+        app = QApplication([])
+        window = MainWindow(model)
+        sys.exit(app.exec_())
